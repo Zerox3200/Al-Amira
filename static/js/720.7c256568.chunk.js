f5076dc44225b0431c072464b48e4030fe270f49
Zerox3200 <ziad.ahmed01020338066@gmail.com>
2024-10-22 22:01:27 +0300

    Updates

---
+++ static/js/720.7c256568.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkalamira=self.webpackChunkalamira||[]).push([[720],{720:(a,t,c)=>{c.d(t,{mm2:()=>l});var h=c(441);function l(a){return(0,h.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"},child:[]},{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"},child:[]}]})(a)}}}]);
+//# sourceMappingURL=720.7c256568.chunk.js.map
